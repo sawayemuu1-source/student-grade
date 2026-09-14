6443ae39d2b14e686346dc1db2f688e9be60b999
sawayemuu1-source <sawayemuu1@gmail.com>
2026-09-14 14:09:31 +0630

    Initial commit: basic grade calculator

---
+++ grade.py
@@ -0,0 +1,8 @@
+name = input("Enter student name: ")
+mark1 = float(input("Enter first subject mark: "))
+mark2 = float(input("Enter second subject mark: "))
+
+average = (mark1 + mark2) / 2
+
+print(f"\nStudent Name: {name}")
+print(f"Average Mark: {average}")
